07f5b6cdbbf83f5d2012a75c8accfa74181c2caf
Stybyk <Topek.man@gmail.com>
2022-04-01 12:45:02 +0200

    Update sensor_data_log.py

--- sensor_data_log.py
+++ sensor_data_log.py
@@ -7,25 +7,25 @@ import Adafruit_DHT
 from influxdb import InfluxDBClient
 
 # Configure InfluxDB connection variables
-host = "10.51.1.39" # My Ubuntu NUC
+host = "127.0.0.1" # My Ubuntu NUC
 port = 8086 # default port
-#user = "rpi-3" # the user/password created for the pi, with write access
-#password = "rpi-3" 
+#user = "rpi" # the user/password created for the pi, with write access
+#password = "rpi" 
 dbname = "sensor_data" # the database we created earlier
-interval = 2 # Sample period in seconds
+interval = 60 # Sample period in seconds
 
 # Create the InfluxDB client object
-#client = InfluxDBClient(host, port, user, password, dbname)
-client = InfluxDBClient(host, port, "", "",  dbname)
+client = InfluxDBClient(host, port, user, password, dbname)
+#client = InfluxDBClient(host, port, "", "",  dbname)
 
 # Enter the sensor details
-sensor = Adafruit_DHT.DHT22
+sensor = Adafruit_DHT.AM2302
 sensor_gpio = 4
 
 # think of measurement as a SQL table, it's not...but...
-measurement = "rpi-dht22"
+measurement = "AM2302"
 # location will be used as a grouping tag later
-location = "office"
+location = "Terminator"
 
 # Run until you get a ctrl^c
 try:
@@ -35,7 +35,7 @@ try:
         #iso = time.ctime()
 	utc = datetime.datetime.utcnow()
         # Print for debugging, uncomment the below line
-        # print("[%s] Temp: %s, Humidity: %s" % (utc, temperature, humidity)) 
+        print("[%s] Temp: %s, Humidity: %s" % (utc, temperature, humidity)) 
         # Create the JSON data structure
         data = [
         {
